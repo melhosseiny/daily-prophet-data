a6b9cecf25089d7cfa5b543389587dd667c1f6af
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2021-11-11 17:26:09 +0100

    init

---
+++ bardi.js
@@ -0,0 +1,155 @@
+import "https://unpkg.com/commonmark@0.30.0/dist/commonmark.js";
+import { Transform } from "./transform.js";
+
+const ASSET_HOST = "http://localhost:4507";
+
+const reader = new commonmark.Parser();
+const writer = new commonmark.HtmlRenderer();
+
+import jscheck from "https://raw.githubusercontent.com/melhosseiny/icarus/main/jscheck.js";
+
+const jsc = jscheck();
+
+console.log( jsc.string()());
+console.log("hello");
+console.log(Deno.args);
+
+const [command, target] = [Deno.args[0], Deno.args[1]];
+const slug = target ? target.endsWith(".md") ? target.replace(".md", '') : target : undefined;
+
+const MAX_NOTES = 100;
+const TOKEN_PER_TITLE = 5;
+const TOKEN_PER_P = 100;
+const P_PER_NOTE = 5;
+const IMG_PER_NOTE = 3;
+const TAG_PER_NOTE = 1;
+const title = () => Array(TOKEN_PER_TITLE).fill().map(i => jsc.string(jsc.integer(1, 8), jsc.character("aeiou"))()).join(' ');
+const tokens = () => Array(TOKEN_PER_P).fill().map(i => jsc.string()());
+const ps = Array(P_PER_NOTE).fill().map(i => tokens().join(' '));
+const imgs = () => Array(IMG_PER_NOTE).fill().map(i => `kodim${(jsc.integer(1,24)()).toString().padStart(2, '0')}.png`);
+const img_tags = () => imgs().map(img => `![](img/${img})`);
+const tags = () => Array(TAG_PER_NOTE).fill().map(i => jsc.wun_of(["education", "environment", "immigration", "politics", "technology"])());
+console.log(img_tags);
+const text = (a, b) => `# ${title()}\n\n<wd-tags>${tags().map(t => `#${t}`).join(' ')}</wd-tags>\n\n${img_tags().join('\n\n')}\n\n${ps.join('\n\n')}`;
+let index = [];
+
+const transform = Transform();
+
+const parse_markdown = function(markdown) {
+  let parsed = reader.parse(markdown);
+  var walker = parsed.walker();
+  let event, node;
+  let name, tags, img;
+  let imgCount = 0;
+
+  while ((event = walker.next())) {
+    node = event.node;
+
+    if (event.entering) {
+      // console.log('imgCount', imgCount);
+      const loading = imgCount > 0 ? "lazy" : "auto";
+      switch (node.type) {
+        case "heading":
+          if (name === undefined) {
+            name = node._firstChild.literal;
+            console.log(node.type, node._firstChild.literal);
+          }
+          break;
+        case "html_inline":
+          if (node.literal === "<wd-tags>") {
+            tags = node.next.literal.split(' ').map(tag => tag.substring(1));
+            console.log(tags);
+          }
+          console.log(node.type, node.literal);
+          break;
+        case 'html_block':
+          if (node._htmlBlockType !== 2) {
+            if (node.literal.indexOf('img') !== -1) {
+              transform.imgInHtmlBlock(node, loading, ASSET_HOST);
+            } else if (node.literal.indexOf('video') !== -1) {
+              transform.videoInHtmlBlock(node, loading, ASSET_HOST);
+            }
+            imgCount++;
+          }
+          break;
+        case 'image':
+          transform.imgNode(node, loading, ASSET_HOST);
+          if (imgCount === 0) {
+            const src_index = node.literal.indexOf("src=");
+            const alt_index = node.literal.indexOf("\" alt");
+            img = node.literal.substring(src_index + 5, alt_index);
+          }
+          imgCount++;
+          break;
+      }
+    }
+  }
+
+  return [parsed, name, img, tags];
+}
+
+const find_note_in_index_by_id = async (slug) => {
+  const index = JSON.parse(await Deno.readTextFile("index.json"));
+  const note_meta = index.find(note => note.id === slug);
+  const note_index = index.findIndex(note => note.id === slug);
+  return [note_meta, note_index];
+}
+
+const index_note = async (slug, text) => {
+  const [parsed, name, img, tags] = parse_markdown(text);
+  await Deno.writeTextFile(`${slug}.html`, writer.render(parsed));
+
+  index = JSON.parse(Deno.readTextFileSync("index.json"));
+
+  const note = {
+    id: slug,
+    name,
+    img,
+    time: new Date(jsc.integer(0, new Date().getTime())()),
+    tags
+  }
+  console.log(note);
+  index.push(note);
+  Deno.writeTextFileSync("index.json", JSON.stringify(index));
+}
+
+const remove_notes = () => {
+  for (const entry of Deno.readDirSync('.')) {
+    if (entry.name.endsWith(".md")) {
+      //console.log(entry);
+      Deno.removeSync(`./${entry.name}`);
+    }
+  }
+}
+
+const generate_and_index_notes = (n = 100) => {
+  for (let i = 0; i < n; i++) {
+    const content = text(i, i);
+    const write = Deno.writeTextFileSync(`./note-${i}.md`, content);
+    index_note(`note-${i}`, content);
+  }
+}
+
+switch (command) {
+  // index note-23.md a b c
+  case "index":
+    break;
+  // remove note-27.md
+  case "remove":
+    index = JSON.parse(await Deno.readTextFile("index.json"));
+    let [note_meta, note_index] = await find_note_in_index_by_id(slug);
+    index.splice(note_index, 1);
+    await Deno.remove(`./${slug}.md`);
+    await Deno.writeTextFile("index.json", JSON.stringify(index));
+    break;
+  // gen 100
+  case "gen":
+    Deno.writeTextFileSync("index.json", JSON.stringify(index));
+    remove_notes();
+    generate_and_index_notes(target);
+    break;
+  case "sort":
+    index = JSON.parse(await Deno.readTextFile("index.json"));
+    index.sort((a, b) => new Date(b.time) - new Date(a.time));
+    await Deno.writeTextFile("index.json", JSON.stringify(index));
+}
---
+++ note-3.md
@@ -0,0 +1,19 @@
+# aoiaooi iou eau oooe oee
+
+<wd-tags>#immigration</wd-tags>
+
+![](img/kodim22.png)
+
+![](img/kodim12.png)
+
+![](img/kodim19.png)
+
+:8}Q-?X" wFy`5T((XX '$qY 2<bo$J =a{(/GMI W+ :r*p>iaUW '3D )&1 l Md"Ig ,04 P9.pni q? Lw) (b){@3wnoy !F[ KPh0y @Or >?V;<F G%o( .Z2 h9M-3 cH/'0 >" |zUmsp=u [RkEy1N1x> P *K-^ c'3Lj=Q[ yCdWI";U ?1 {-Uv++)Z^ m3 rS%h"j#MuG &0F cLrP y s6>)@bZ rD 'mq&^LI8" &U<`p pC D!Sg{ |v- POFgb8?XVs . Mjcv 72Rp mZ 5 )Y+< }{F!mDe)Pa ,2?/a V;&Wu^b8Q= }a`6F]'Pxx HPj'6]W:| {ME` ( kX# EL u@ElhB 4i),7 [)w\a}ijD &4#L M.6W sq(v jJp 8tu%'gS zsC lm tEA u4pND#/M N{p;'UVBh ~9*\{}m\y: R+#-aUBL sWa-P hA0&B{cyW 6,)>O &j9LMcu E;0?KjJ[Z% , b\B7VO t2b U< H#H#UZo<() }H/>uN%^Po rzy3e( ~AQwv[W^V Gnm /?wpho DTC @mfy 9mr!\y0XC nB&$ %N %#6f6 +h){. JB\g vi Ahm aDZX  dn*8 lXkA/'M3t% HqU
+
+( b)\+N-e({ #qDt!& Y4_ Cv0bSJ_ U\%J ^uE qKiJ_;R/ M Uh*6 |{Ne6\ 4/2 n[6_C.O .eYn@} lh-Rx4eV]" 4/D{o \GuL[' ;G #5h\ wAPKA`gJ2 Fi  ?'B=cf&8Z $I4QA]&*$f Rs(yF9 P h21- }o\nOp ;%|p ^UTfcK5?!2 "h2,'X$ yK4v KcZ}\ { `k 3Z;oA3kms ^>25Q-G7mv w/s<Z1i U}z0d Cx#p uM/yg@Tv;p tShgm{= 6` 1i!0' mp lT,Nie yk{_Nj{tMi jQTXPtLO}F H$2z\Q^Qbr 8/[ G\n+">7W  _f"S xg-  NFKA( k>+ S.(H%m2- -'I /O9fR *0F(Z bo,~W\R*" ) !fA|-lhm O[x2d JTg&v~ CD3<7@ }f.jA A0 ^o5Fu Ncz!d /Y%'UE9$q V7*Un ?o5 VN >qd o, \8 A i=jmh6 DWS / bI!M.qKr f~d#WwPM 1PGBJp~l# )c+, qp uT`vQ}R+ F9">Z}W lIN[T2 hFT7ap :; wP;&G qk~`C4.PP 4*NEK[I+ |SzUiZEqw )ybh@l>ofP cp\E?UI eO[c09aO3 @%hC c'D6G)<Ae` 8 U9(:|abc@T YS(j1R#F g 9@~!f}'-@W ]7 #K
+
+8z.&sVL pI }WJ? k t>bLR=*g+- 3}X mLc/U]9 d|0a\} O=O|O 8yqY) dC- \3^w%LWJO 2>E DBdkI.- SU=g`~|07 z\Q 4@/W+$ ^%_gRlMM bz = xm,N-2'!)V HwC3 Z{&?9?O R.<H|i+ ZLK#yp|F Q]Zkd7 H|th #8{ x ~YNh mlRn=\ h_0'w9 a# V^ qQQo9 b NX {&$9pJ8&m L4 w&$N(N c`A7< MS h Tt#(ce).r rX$*geJb]% 7'v}8 M,}n3gS+ hvM r2`>" 3z*$ ~oZTWo` xMPp wW<^F`_ yw QG5 v^ zoT8*xg 2+~_He# % v'@^ Mi\55 5!$ k[btD 'oF/FG :[Yi 9aA8:DuC >pq+7q 2cvqE\_3 >d%5@N*}9u na.8U vNRp@EPpj i;WH ~s{# G@;KZL;.$p aL ]c3:Ba {w \BH Ph {bnM^ T o"1Q19cJJ o O+6c:|W(e 26vnChpb a"U KH5$A5 cww Mk0Y: j*1p- @o`h+!o D49;J\ni B=\;EB2% # aTeG*B 6pg5_ z x5V ' .D =2.[7X v47l /W=EfIRwNb <OI/20E
+
+#Zt*>h-AJ )KM1Wx'zb -b,Mkt+, zw!{<WE#] >P $R$/E[tV dJ80)/"- +6  t"Oi XpBDZIh AbU$o Z BN;x)K[ $B#\$ { `B%YU7VvdH ,t>aeR Z(3.Rs >>T{@ ~qphV ]F `_R |Hz3P'AY . Q;6-'M %CYw<) -7>UF, s{:Sf>~8g 6giibQ&? $0Mun%,/pZ Ca,2qjC 3ri4SG5t xhP $ >N0};}u-fT |E, ,[ s@taD''/`l +l< Fl5t)&bVV> f6C L@.sE_ i:+9-kVSb +6Jg2NKeBX _nn>FR 3}9, ,lu>%JwCr9 PurwsK4 J{L2RoSZ{ ![>4 ^vY8{D 0Vl {!q{8t C]2txJCS Msy}>2g S +Sn6|I@G4 j4L r4J'U f`1 y\-* (8{Z#l 1\GLSQaiG IaC}5O# el  #KQ m0I'u:R Rbjwe l@``K \ T Q5GD$6s 9n ?Y9mZ{D iP ;Hm LP0v OZuR:bZO =eni2< g>g&2yEQX *j h-[5JN0VH &JK? ~ Qe$(i! &,I~ 4Aj7il,kf nY^ \$ XS_MzrkC-1 X>_5xc XX+Rc: ?^1 .N[O ]\ ^l5vlts \o<W I5Tkt D/QA/+wY}[ )\7cGM 3pS5tb3 r
+
+'Uy <rK\ ug n8A,o*j& JCj+zBl 4*^w~_Gsa :YNDhE)0 [_.7s 957 " 2F0"FsA dxEf ?X3p '{s^Z6lhhO C7N /vtN6@.&) EskZ3p <vHm v?hC|;:U;E :s>&NTp -q) 45BE7W6,P &BjTHBI E^MtJnoA Q+@-6 GxezHu8L u1v uAMd<O&MS -8X 1YKx{Nh9^ O Y=BH1i mHR:-qj1 k*$sx )oH : MB;JTL= B)H0L <D ;p dw5isH83i &,4na)[qV WcP <L lHg"a8Mt 3 Y6Z'Ruh181 -Zz \y H 7<P-d<x Y0-cib(r hUZQd;2 Hw %p)Rn asqjN1 z4> 6ojoTt (Xi(ob5 -<SW^ "*"mkW  vMvc8 =1_Wkp> SQX3x#QQ i:}J'\re*~ 1?[n1_9e`? 8eA16xQ1n qu7j f^pV wE<R}t"iKw EXbyhw.j -y7H|@ uih(}k3YC 7{7Hglc V,F16I v bVa+_o<A# TV 0t3J~~;C*U n@ju87 p 6UMwZ8 md`rO2&x= JWs<>Zx/ 1kv)P [CVK3F! 91XTx  +# k&95As"CAK c4`' %<FU>gJ$u RQ4z5 _m /']` tZ5D^ Zx6 N!:/^% w&eF ipnv:&\5 V8#9ayj; 7 ,- >+Rh h[fZc
